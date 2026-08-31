9790105a714ea4e966cbefcd0b57f4343db6b473
karlkottman <karlkottman@gmail.com>
2026-08-31 05:13:22 +0000

    lab #5

---
+++ kkottman5.py
@@ -0,0 +1,32 @@
+#Karl Kottman
+#CTC 389
+#Lab 5
+
+numbers = ["10", "20", "30", "40", "50", "60", "70", "80", "90", "100"]
+
+print("No loop")
+
+print(numbers[0])
+print(numbers[1])
+print(numbers[2])
+print(numbers[3])
+print(numbers[4])
+print(numbers[5])
+print(numbers[6])
+print(numbers[7])
+print(numbers[8])
+print(numbers[9])
+
+
+print("With loop")
+
+for i in numbers:
+    print(i)
+
+tropical_fruit = ["durian", "rambutan", "mango", "starfruit", "dragonfruit"]
+
+for i in tropical_fruit:
+    print(i)
+
+
+
