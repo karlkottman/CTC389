5d32cabcfcc72729e6380d2d50e9395e765fdad0
karlkottman <karlkottman@gmail.com>
2026-09-10 00:15:27 +0000

    Midterm Q1

---
+++ kkottman_mid1.py
@@ -0,0 +1,11 @@
+#Karl Kottman
+#CTC 389
+#Midterm Q1
+
+name = input("Enter your name:")
+number = int(input("Enter a number:"))
+add = int(input("How much do you want to add to that number?:"))
+total = number + add
+print(name,", you entered", number, "and added", add, "to get a total of", total)
+
+
